d49e6177229400a745b4c32c76bca0da9d6c4885
Nurseit Kalbaev <kalban1991@gmail.com>
2021-02-25 20:06:07 -0800

    UI of the my-chat done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h1>Starting project</h1>
+      <div className="app_body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCeTh-YwUZpI-huvoU_3T0AY90z95Ggit8",
+  authDomain: "my-chat-9a47a.firebaseapp.com",
+  projectId: "my-chat-9a47a",
+  storageBucket: "my-chat-9a47a.appspot.com",
+  messagingSenderId: "534045748097",
+  appId: "1:534045748097:web:a3e216f295d6e55004d680",
+};
